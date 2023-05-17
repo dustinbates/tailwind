4082ffd76938d4edd8b8dd0d6553eeaa4bc67843
Dustin Bates <dustin.bates98@gmail.com>
2023-05-16 21:20:52 -0600

    commit

---
+++ latest/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="flex flex-col md:flex md:flex-row justify-center p-72">
+    <button class="bg-indigo-500 text-3xl text-white rounded-xl p-2 cursor-pointer">Hello Borld</button>
+    <p class="bg-red-500 text-2xl text-white rounded p-3">Peanus Weanus</p>
+    <h2 class="bg-green-500 text-9xl text-black rounded-lg p-5">WHAT</h2>
+    <h3 class="bg-yellow-500 text-xs rounded p-1">THE</h3>
+    <button class="bg-purple-500 rounded p-5 hover:bg-purple-900 hover:text-white hover:p-3 transition-all ease-linear focus:outline-yellow-600 cursor-pointer active:outline-yellow-600" tabindex="0">FUCK</button>
+    <body >
+      <p class="bg-fuchsia-600 text-5xl rounded-lg">PING PONG POG</p>
+    </body>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
